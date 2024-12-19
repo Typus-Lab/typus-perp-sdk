35e9844277ce43287db914336da36505472d9ecb
Wayne Kuo <wayne.k@typuslab.com>
2024-12-19 14:14:09 +0800

    init

---
+++ src/typus_stake_pool/index.ts
@@ -0,0 +1,3 @@
+export const PACKAGE_ID = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
+export const PUBLISHED_AT = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
+export const PKG_V1 = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";

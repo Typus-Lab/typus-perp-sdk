4a948219b802b729b8028073921fef27e39310b8
Wayne Kuo <wayne.k@typuslab.com>
2025-01-03 18:14:46 +0800

    update index

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ import dotenv from "dotenv";
 // import .env file if exists
 dotenv.config();
 export const NETWORK = process.env.CLUSTER == "mainnet" ? "MAINNET" : "TESTNET";
+
+console.log(`Initializing Typus Perp SDK for ${NETWORK}`);

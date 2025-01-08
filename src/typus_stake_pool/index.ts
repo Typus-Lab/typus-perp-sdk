9c9df16c239eabe2ecb2c94d91d2cd12c9151fdb
Wayne Kuo <wayne.k@typuslab.com>
2025-01-08 14:55:43 +0800

    update path

--- src/typus_stake_pool/index.ts
+++ src/typus_stake_pool/index.ts
@@ -1,4 +1,4 @@
-import { NETWORK } from "src";
+import { NETWORK } from "../index";
 
 export const PACKAGE_ID = NETWORK == "MAINNET" ? "" : "0x2861ec51c70e3bd8ff2372e210acb7151ece286d17f43447308d5f0ebbe8603b";
 export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0x2861ec51c70e3bd8ff2372e210acb7151ece286d17f43447308d5f0ebbe8603b";

1c5b69f263d0905d6bca11ef0d10537d9828e9a9
Wayne Kuo <wayne.k@typuslab.com>
2025-01-22 19:27:04 +0800

    update history

--- src/user/history.ts
+++ src/user/history.ts
@@ -3,7 +3,13 @@ import { Token } from "@typus/typus-sdk/dist/src/utils";
 import { NETWORK } from "../index";
 import { PKG_V1 as PERP_PACKAGE_ID } from "../typus_perp/index";
 import { OrderFilledEvent } from "../typus_perp/position/structs";
-import { CancelTradingOrderEvent, CreateTradingOrderEvent } from "../typus_perp/trading/structs";
+import {
+    CancelTradingOrderEvent,
+    CreateTradingOrderEvent,
+    IncreaseCollateralEvent,
+    ReleaseCollateralEvent,
+} from "../typus_perp/trading/structs";
+import { SwapEvent } from "src/typus_perp/lp-pool/structs";
 
 type actionType =
     | "Place Order"
@@ -27,14 +33,14 @@ interface Event {
     order_id: string | undefined;
     position_id: string | undefined;
     market: string;
-    side: sideType;
-    order_type: orderType;
+    side: sideType | undefined;
+    order_type: orderType | undefined;
     status: statusType;
-    size: number;
+    size: number | undefined;
     base_token: TOKEN;
-    collateral: number;
+    collateral: number | undefined;
     collateral_token: TOKEN;
-    price: number;
+    price: number | undefined;
     realized_pnl: number | undefined;
     timestamp: string;
     tx_digest: string;
@@ -82,7 +88,7 @@ export async function getUserHistory(sender: string) {
 
                 var e: Event = {
                     action: "Place Order",
-                    order_id: undefined, // TODO: add order_id
+                    order_id: json.order_id,
                     position_id: json.linked_position_id,
                     market: `${base_token}/USD`,
                     side: json.is_long ? "Long" : "Short",
@@ -106,12 +112,16 @@ export async function getUserHistory(sender: string) {
 
                 var size = Number(json.filled_size) / 10 ** assetToDecimal(base_token)!;
                 var price = json.filled_price;
-                var action;
+                var action: actionType;
+                var related: Event | undefined;
 
                 if (json.linked_position_id) {
                     action = "Order Filled (Close Position)";
+                    related = events.findLast((e) => e.position_id === json.linked_position_id);
+                    // the "Place Order" is emit after Order Filled if filled immediately
                 } else {
                     action = "Order Filled (Open Position)";
+                    related = events.findLast((e) => e.order_id === json.order_id);
                 }
 
                 var realized_trading_fee = Number(json.realized_trading_fee) + Number(json.realized_borrow_fee);
@@ -121,12 +131,12 @@ export async function getUserHistory(sender: string) {
                     order_id: json.order_id,
                     position_id: json.linked_position_id ?? json.new_position_id,
                     market: `${base_token}/USD`,
-                    side: "Long", // TODO: search order_id
-                    order_type: "Market", // TODO: search order_id
+                    side: related?.side,
+                    order_type: related?.order_type,
                     status: json.filled ? "Filled" : "Open",
                     size,
                     base_token,
-                    collateral: 0, // TODO: search order_id
+                    collateral: related?.collateral,
                     collateral_token,
                     price: Number(price) / 10 ** 8, // WARNING: fixed decimal
                     realized_pnl: (0 - realized_trading_fee) / 10 ** assetToDecimal(base_token)!,
@@ -135,17 +145,19 @@ export async function getUserHistory(sender: string) {
                 };
                 events.push(e);
                 break;
+
             case CancelTradingOrderEvent.$typeName:
                 var base_token = typeArgToToken(json.base_token.name) as TOKEN;
                 var collateral_token = typeArgToToken(json.collateral_token.name) as TOKEN;
+                var related = events.findLast((e) => e.order_id === json.order_id);
 
                 var e: Event = {
                     action: "Cancel Order",
                     order_id: json.order_id,
                     position_id: undefined,
                     market: `${base_token}/USD`,
-                    side: "Long", // TODO: search order_id
-                    order_type: "Market", // TODO: search order_id
+                    side: related?.side,
+                    order_type: related?.order_type,
                     status: "Canceled",
                     size: 0,
                     base_token,
@@ -153,6 +165,66 @@ export async function getUserHistory(sender: string) {
                     collateral_token,
                     price: Number(json.trigger_price) / 10 ** 8, // WARNING: fixed decimal
                     realized_pnl: undefined,
+                    timestamp,
+                    tx_digest,
+                };
+                events.push(e);
+                break;
+
+            case IncreaseCollateralEvent.$typeName:
+            case ReleaseCollateralEvent.$typeName:
+                var base_token = typeArgToToken(json.base_token.name) as TOKEN;
+                var collateral_token = typeArgToToken(json.collateral_token.name) as TOKEN;
+                var related = events.findLast((e) => e.position_id === json.position_id);
+
+                var collateral: number;
+                if (json.increased_collateral_amount) {
+                    collateral = Number(json.increased_collateral_amount);
+                } else {
+                    collateral = Number(json.released_collateral_amount) * -1;
+                }
+
+                var e: Event = {
+                    action: "Modify Collateral",
+                    order_id: undefined,
+                    position_id: json.position_id,
+                    market: `${base_token}/USD`,
+                    side: related?.side,
+                    order_type: related?.order_type,
+                    status: "Filled",
+                    size: related?.size,
+                    base_token,
+                    collateral: collateral / 10 ** assetToDecimal(collateral_token)!, // WARNING: fixed decimal
+                    collateral_token,
+                    price: related?.price,
+                    realized_pnl: undefined,
+                    timestamp,
+                    tx_digest,
+                };
+                events.push(e);
+                break;
+
+            case SwapEvent.$typeName:
+                var from_token = typeArgToToken(json.from_token_type.name) as TOKEN;
+                var to_token = typeArgToToken(json.to_token_type.name) as TOKEN;
+
+                var from_price = Number(json.oracle_price_from_token);
+                var to_price = Number(json.oracle_price_to_token);
+
+                var e: Event = {
+                    action: "Swap",
+                    order_id: undefined,
+                    position_id: undefined,
+                    market: `${from_token}/${to_token}`,
+                    side: undefined,
+                    order_type: "Market",
+                    status: "Filled",
+                    size: Number(json.actual_to_amount) / 10 ** assetToDecimal(to_token)!,
+                    base_token: to_token,
+                    collateral: Number(json.from_amount) / 10 ** assetToDecimal(from_token)!,
+                    collateral_token: from_token,
+                    price: from_price / to_price,
+                    realized_pnl: -Number(json.fee_amount_usd) / 10 ** 6,
                     timestamp,
                     tx_digest,
                 };

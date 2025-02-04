cb66f5be2f606f892682a59f1e02798a55f7c16f
Wayne Kuo <wayne.k@typuslab.com>
2025-02-04 16:39:48 +0800

    Update getMarkets.ts

--- test/fetch/getMarkets.ts
+++ test/fetch/getMarkets.ts
@@ -1,11 +1,8 @@
 import { TypusConfig } from "@typus/typus-sdk/dist/src/utils";
-import { SuiClient } from "@mysten/sui/client";
 import { getMarkets } from "src";
 
 (async () => {
     let config = await TypusConfig.default("TESTNET", null);
-    let provider = new SuiClient({ url: config.rpcEndpoint });
-
     let markets = await getMarkets(config, { indexes: ["0"] });
     console.log(JSON.stringify(markets, null, 2));
 })();

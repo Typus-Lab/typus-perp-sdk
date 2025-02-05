c2e1e274ecb368fac370d0b6abadf81bcef34c6b
Wayne Kuo <wayne.k@typuslab.com>
2025-02-05 16:42:23 +0800

    getLiquidationPriceAndPnl

--- src/typus_perp/index.ts
+++ src/typus_perp/index.ts
@@ -1,5 +1,5 @@
 import { NETWORK } from "../index";
 
 export const PACKAGE_ID = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";
-export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";
+export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0xdba9a6e29b91b391da4a539ee56ff18e5ae5d262e3885c1ce69214dd94a402f8";
 export const PKG_V1 = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";

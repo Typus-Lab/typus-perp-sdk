90b3d6510927ae2940ce9f0417c7a41c2a49210e
alvin7878 <103410194+alvin7878@users.noreply.github.com>
2025-02-06 18:24:37 +0900

    update PUBLISHED_AT

--- src/typus_perp/index.ts
+++ src/typus_perp/index.ts
@@ -1,5 +1,5 @@
 import { NETWORK } from "../index";
 
 export const PACKAGE_ID = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";
-export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0x002be1fdabc2d7e0de0244d55cc967a2a9bbd19e8fb7e9e97c6118576d2ce767";
+export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0xcab4ab2f01471de25e48b6677dda9aeb6ccce9a36a07e9f5f60ccf275baed3a8";
 export const PKG_V1 = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";

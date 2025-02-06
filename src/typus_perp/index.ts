2d9c10040629079385d8dce9fd85d1422a4ecb18
Wayne Kuo <wayne.k@typuslab.com>
2025-02-06 14:37:29 +0800

    update PUBLISHED_AT

--- src/typus_perp/index.ts
+++ src/typus_perp/index.ts
@@ -1,5 +1,5 @@
 import { NETWORK } from "../index";
 
 export const PACKAGE_ID = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";
-export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0xad1d40092cf67d5cc50e35efd1d74bfe3c08808cd0ccbf760dc3dec57d9ec983";
+export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0x002be1fdabc2d7e0de0244d55cc967a2a9bbd19e8fb7e9e97c6118576d2ce767";
 export const PKG_V1 = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";

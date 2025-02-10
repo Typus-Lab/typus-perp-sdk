45f510c4d5b274b791f922cb5b104b97dbbae626
alvin7878 <103410194+alvin7878@users.noreply.github.com>
2025-02-10 19:03:49 +0900

    update perp PUBLISHED_AT

--- src/typus_perp/index.ts
+++ src/typus_perp/index.ts
@@ -1,5 +1,5 @@
 import { NETWORK } from "../index";
 
 export const PACKAGE_ID = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";
-export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0xcab4ab2f01471de25e48b6677dda9aeb6ccce9a36a07e9f5f60ccf275baed3a8";
+export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0x16075533726388ef93a1d036fe192a9a367ce6f80e3cb4d462019a8b5c8bbe0e";
 export const PKG_V1 = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";

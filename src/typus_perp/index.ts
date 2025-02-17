238398861f55fdfe5c4d6f0ea64dbdeb151b284c
Wayne Kuo <wayne.k@typuslab.com>
2025-02-17 14:37:30 +0800

    upgrade

--- src/typus_perp/index.ts
+++ src/typus_perp/index.ts
@@ -1,5 +1,5 @@
 import { NETWORK } from "../index";
 
 export const PACKAGE_ID = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";
-export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0xeb3331e74efa1e2e51d665fe1369c4f721eaabbb3974ffd75503a84ab4409c42";
+export const PUBLISHED_AT = NETWORK == "MAINNET" ? "" : "0xbeb23d2d58881dbe45c2493d20d86e079600e367d7b5de5a28e5ec10acd28793";
 export const PKG_V1 = NETWORK == "MAINNET" ? "" : "0x7c19f81d9d411e78305d7af8dad25317c56bb449fede8a78b6021232478f0509";

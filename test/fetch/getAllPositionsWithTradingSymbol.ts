7d480d0c331c269e642e5be6c268554eb1927818
Zhou180 <sam8811288.cs08@nctu.edu.tw>
2025-05-16 18:42:09 +0800

    implement getAllPositionsWithTradingSymbol function and add initial test

---
+++ test/fetch/getAllPositionsWithTradingSymbol.ts
@@ -0,0 +1,14 @@
+import { TOKEN } from "@typus/typus-sdk/dist/src/constants";
+import { TypusConfig } from "@typus/typus-sdk/dist/src/utils";
+import { getAllPositionsWithTradingSymbol, NETWORK } from "src";
+
+(async () => {
+    let config = await TypusConfig.default(NETWORK, null);
+
+    let baseToken: TOKEN = "SUI";
+
+    let res = await getAllPositionsWithTradingSymbol(config, {
+        baseToken,
+    });
+    console.log(res);
+})();

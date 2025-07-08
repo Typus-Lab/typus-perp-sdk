a85e5b38a7fd7270ee1b39454fd9de5007b13f50
Wayne Kuo <wayne.k@typuslab.com>
2025-07-08 16:13:50 +0800

    getUserPnlFromSentio

--- src/api/sentio.ts
+++ src/api/sentio.ts
@@ -135,6 +135,10 @@ function now() {
     return Math.round(Date.now() / 1000);
 }
 
+function parseTimestamp(s: string) {
+    return Math.round(Date.parse(s) / 1000);
+}
+
 export async function getTlpFeeFromSentio(fromTimestamp: number, toTimestamp?: number): Promise<number> {
     let apiUrl = "https://app.sentio.xyz/api/v1/insights/typus/typus_perp_mainnet/query";
     let requestData = {
@@ -342,9 +346,97 @@ export async function getTlpPriceFromSentio(fromTimestamp: number, toTimestamp?:
     return samples[0].values;
 }
 
+export async function getUserPnlFromSentio(startTimestamp: number, endTimestamp: number, userAddress?: string): Promise<any[]> {
+    let apiUrl = "https://app.sentio.xyz/api/v1/analytics/typus/typus_perp_mainnet/sql/execute";
+
+    let userFilter = "";
+    if (userAddress) {
+        userFilter = `AND address == '${userAddress}'`;
+    }
+
+    let requestData = {
+        version: 25,
+        sqlQuery: {
+            sql: `
+            WITH
+            Pnl_history as (
+                SELECT
+                timestamp as time,
+                CAST( realized_pnl AS Float64 ) as Trader_PnlUSD,
+                distinct_id as address
+                from OrderFilled
+                WHERE timestamp >= ${startTimestamp} and timestamp < ${endTimestamp}
+                UNION ALL
+                SELECT
+                timestamp as time,
+                CAST(value_for_lp_pool_usd + liquidator_fee_usd AS Float64) * (-1) as Trader_PnlUSD,
+                distinct_id as address
+                from Liquidate -- collateral liquidate, liquidate fee不算
+                WHERE timestamp >= ${startTimestamp} and timestamp < ${endTimestamp}
+                UNION ALL
+                SELECT
+                timestamp as time,
+                CAST(realized_funding_fee_usd AS Float64) * (-1) as Trader_PnlUSD,
+                distinct_id as address
+                from 'RealizeFunding' -- funding fee 正的是 user paid to pool
+                WHERE timestamp >= ${startTimestamp} and timestamp < ${endTimestamp}
+                UNION ALL
+                SELECT
+                timestamp as time,
+                CAST(
+                    user_remaining_in_usd + (realized_loss_value - exercise_balance_value) * (user_remaining_in_usd / user_remaining_value) AS Float64
+                ) as Trader_PnlUSD,
+                distinct_id as address
+                from 'RealizeOption'
+                WHERE timestamp >= ${startTimestamp} and timestamp < ${endTimestamp}
+            ),
+            sum_Trader_PnlUSD as (
+                SELECT
+                sum(Trader_PnlUSD) as Trader_PnlUSD,
+                address
+                from Pnl_history
+                GROUP BY address
+            )
+            SELECT
+            *
+            from sum_Trader_PnlUSD
+            where address not in (
+                '0xc9ea1b9c3542551189cf26a08f5ca5ed7964aef34c14d06c888d30c8d91867e4',
+                '0x83d2cb640ee252bae6b01bd6104c4afc69071e67b688db85a029ce452c61f11c',
+                '0x39770d149a9bc9d9639314fca2c380e9061c0d230737635762c5bcc61dee13d0',
+                '0x834d17f7b2e167cae224325a17b49babffe168182c418caa0c63a92d6f70b83a',
+                '0xd35ae21660aee607ee21e104d093d03643d36efbf79df669092c4411308ed2e2',
+                '0xe6cbba68c446f52cf8211c12ae79179233c8f9cec5e0b5008418ec339ca72fea'
+                )
+            ${userFilter}
+            order by Trader_PnlUSD desc
+            `,
+            size: 100,
+        },
+    };
+
+    let jsonData = JSON.stringify(requestData);
+
+    let response = await fetch(apiUrl, {
+        method: "POST",
+        headers,
+        body: jsonData,
+    });
+
+    let data = await response.json();
+    console.log(data);
+
+    if (data.result) {
+        return data.result.rows as any[];
+    } else {
+        return [];
+    }
+}
+
 // getVolumeFromSentio();
 // getAccumulatedUser();
 // getTradingVolumeFromSentio(1747008000, 1, 1747011600);
 // getTlpPriceFromSentio(now() - 3600 * 24 * 30).then((x) => console.dir(x, { depth: null }));
 // getTotalVolumeFromSentio(now() - 3600 * 24 * 30).then((x) => console.log(x));
 // getTlpFeeFromSentio(now() - 3600 * 24 * 30).then((x) => console.log(x));
+// getUserPnlFromSentio(parseTimestamp("2025-06-24 11:00:00"), parseTimestamp("2025-07-08 11:00:00")).then((x) => console.log(x));

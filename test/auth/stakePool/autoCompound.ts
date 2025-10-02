e05ad73296828086b7b3328462509da3b546d367
Wayne Kuo <wayne.k@typuslab.com>
2025-10-02 14:01:57 +0800

    Create autoCompound.ts

---
+++ test/auth/stakePool/autoCompound.ts
@@ -0,0 +1,31 @@
+import "@typus/typus-sdk/dist/src/utils/load_env";
+import { TypusConfig } from "@typus/typus-sdk/dist/src/utils";
+import { SuiClient } from "@mysten/sui/client";
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import { Transaction } from "@mysten/sui/transactions";
+import { MARKET, NETWORK, PERP_VERSION, STAKE_POOL, STAKE_POOL_VERSION, TLP_TOKEN } from "src";
+import { autoCompound } from "src/typus_stake_pool/stake-pool/functions";
+
+import mne from "mnemonic.json";
+import { CLOCK } from "@typus/typus-sdk/dist/src/constants";
+
+(async () => {
+    let keypair = Ed25519Keypair.deriveKeypair(String(mne.MNEMONIC));
+    let config = await TypusConfig.default(NETWORK, null);
+    let provider = new SuiClient({ url: config.rpcEndpoint });
+
+    let user = keypair.toSuiAddress();
+    console.log(user);
+
+    var tx = new Transaction();
+
+    autoCompound(tx, TLP_TOKEN, {
+        version: STAKE_POOL_VERSION,
+        registry: STAKE_POOL,
+        index: BigInt(0),
+        clock: CLOCK,
+    });
+
+    let res = await provider.signAndExecuteTransaction({ signer: keypair, transaction: tx });
+    console.log(res);
+})();

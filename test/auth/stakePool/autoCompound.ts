cfb937fbb517c336203f66fd812d5db1a697d993
Wayne Kuo <wayne.k@typuslab.com>
2026-02-23 17:31:55 +0800

    Update autoCompound.ts

--- test/auth/stakePool/autoCompound.ts
+++ test/auth/stakePool/autoCompound.ts
@@ -4,30 +4,35 @@ import { TypusClient } from "src/client";
 import { SuiClient } from "@mysten/sui/client";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { Transaction } from "@mysten/sui/transactions";
-import { MARKET, NETWORK, PERP_VERSION, STAKE_POOL, STAKE_POOL_VERSION, TLP_TOKEN } from "src";
-import { autoCompound } from "src/typus_stake_pool/stake-pool/functions";
-
-import mne from "mnemonic.json";
-import { CLOCK } from "@typus/typus-sdk/dist/src/constants";
+import { getStakePools, MARKET, NETWORK, PERP_VERSION, STAKE_POOL, STAKE_POOL_VERSION } from "src";
+import { autoCompound } from "src/generated/typus_stake_pool/stake_pool";
+import { normalizeStructTag } from "@mysten/sui/utils";
 
 (async () => {
-    let keypair = Ed25519Keypair.deriveKeypair(String(mne.MNEMONIC));
+    let keypair = Ed25519Keypair.deriveKeypair(String(process.env.AUTH_MNEMONIC));
     let config = await TypusConfig.default(NETWORK, null);
     let client = new TypusClient(config);
-    let provider = new SuiClient({ url: config.rpcEndpoint });
 
     let user = keypair.toSuiAddress();
     console.log(user);
 
+    const index = 0;
+    let stakePools = await getStakePools(client);
+    let stakePool = stakePools[index];
+    console.log(stakePool);
+
     var tx = new Transaction();
+    tx.add(
+        autoCompound({
+            arguments: {
+                version: STAKE_POOL_VERSION,
+                registry: STAKE_POOL,
+                index: BigInt(index),
+            },
+            typeArguments: [normalizeStructTag(stakePool.pool_info.stake_token.name)],
+        })
+    );
 
-    autoCompound(tx, TLP_TOKEN, {
-        version: STAKE_POOL_VERSION,
-        registry: STAKE_POOL,
-        index: BigInt(0),
-        clock: CLOCK,
-    });
-
-    let res = await provider.signAndExecuteTransaction({ signer: keypair, transaction: tx });
+    let res = await client.signAndExecuteTransaction({ signer: keypair, transaction: tx });
     console.log(res);
 })();
